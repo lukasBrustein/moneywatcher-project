ac37ef87210b3914b247875fd13bb98d5baeb9d0
lukasBrustein <40600537+lukasBrustein@users.noreply.github.com>
2020-09-23 22:24:16 +0200

    Update Toolbar.js

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -5,31 +5,42 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import{ NavLink } from 'react-router-dom';
 import  Button  from '@material-ui/core/Button';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
-import NotificationsNoneIcon from '@material-ui/icons/Notifications';
+import NotificationsOutlinedIcon from '@material-ui/icons/NotificationsOutlined';
+import IconButton from '@material-ui/core/IconButton';
+import CalendarTodayOutlinedIcon from '@material-ui/icons/CalendarTodayOutlined';
+import PowerSettingsNewOutlinedIcon from '@material-ui/icons/PowerSettingsNewOutlined';
+
+
+
 const toolbar = (props) => (
     <header>
         <Navbar bg="light" expand="lg">
-  <Navbar.Brand href="./home">MoneyWatcher</Navbar.Brand>
+  <Navbar.Brand href="./home" style={{fontFamily: 'Helvetica'}}>MoneyWatcher</Navbar.Brand>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="mr-auto">
       <Nav.Link href="#header"></Nav.Link>
     </Nav>
     <Nav.Link href="/notifications">
-    <NotificationsNoneIcon />
+      <IconButton color="primary">
+            <NotificationsOutlinedIcon/>
+      </IconButton>
     </Nav.Link>
-      <NavDropdown title="Lukas Brustein" id="basic-nav-dropdown">
+    <Nav.Link href="/calendar">
+      <IconButton color="primary">
+        <CalendarTodayOutlinedIcon />
+      </IconButton>
+    </Nav.Link>
+    <Button color="primary">
+      <NavDropdown title="Lukas Brustein" id="basic-nav-dropdown" style={{fontFamily: 'Helvetica'}}>
         <Nav.Link href="/profile">
-        
-     
-           <NavDropdown.Item as= {Button}>
-                Profil
-            </NavDropdown.Item>
+           <NavDropdown.Item as= {Button}><AccountCircleIcon/>Profil</NavDropdown.Item>
         </Nav.Link>
         <NavDropdown.Divider />
-        <NavDropdown.Item as= {Button}>Logout</NavDropdown.Item>
+        <NavDropdown.Item as= {Button}><PowerSettingsNewOutlinedIcon />Logout</NavDropdown.Item>
       </NavDropdown>
-      <AccountCircleIcon style={{width:'3%', height:'3%'}}/>
+      </Button>
+     
   </Navbar.Collapse>
   
 </Navbar>
