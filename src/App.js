69f9a0da42e13d6514a115b84abd76b054f67d9e
lukasBrustein <40600537+lukasBrustein@users.noreply.github.com>
2020-09-23 22:24:02 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Toolbar from './components/Navigation/Toolbar/Toolbar';
+import Sidenav from './components/Navigation/Sidenav/Sidenav';
 
 function App() {
   return (
