2def85f4fa4cd78627be79382f0130ab4578ed63
lukasBrustein <40600537+lukasBrustein@users.noreply.github.com>
2020-09-28 19:58:34 +0200

    Update Layout.js

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -2,14 +2,16 @@ import React, {Component} from 'react';
 import Aux from './../Auxiliary/Auxiliary';
 import Toolbar from './../../components/Navigation/Toolbar/Toolbar';
 import Sidenav from './../../components/Navigation/Sidenav/Sidenav';
+import Topnav from './../../components/Navigation/Topnav/Topnav';
 
 class Layout extends React.Component {
     render() {
         return (
         <Aux> 
             <Toolbar />
+            <Topnav style={{width:"5px"}}/>
             <Sidenav />
-            
+          
         </Aux>
         )
     }
