50431abbf195ca3509d8ce3d21d0216a27265189
lukasBrustein <40600537+lukasBrustein@users.noreply.github.com>
2020-09-28 19:58:00 +0200

    Commit

---
+++ src/components/Navigation/Topnav/Topnav.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+
+const topnav = () => (
+    <AppBar position="static" style={{marginLeft: "12vw", backgroundColor:"white", maxWidth:"88%"}}>
+        <h4 style={{color:"grey"}}>Dashboard</h4>
+    </AppBar>
+)
+export default topnav;
